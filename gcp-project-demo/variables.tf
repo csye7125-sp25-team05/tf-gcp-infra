06b862c2282b31d5fa8e6512218ce747ac0f0876
Mayuresh Satao <msatao75@gmail.com>
2025-02-07 19:32:51 -0500

    gcp-project-demo added, and gcp subdomain created

---
+++ gcp-project-demo/variables.tf
@@ -0,0 +1,89 @@
+# variable "project_name" {
+#   type        = string
+#   description = "The name of the project"
+# }
+
+variable "organization_id" {
+  type        = string
+  description = "The ID of the organization"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The ID of the existing GCP project"
+}
+
+variable "billing_account" {
+  type        = string
+  description = "The billing account ID"
+}
+
+variable "zones" {
+  type    = list(string)
+  default = ["us-east1-a", "us-east1-b", "us-east1-c"]
+}
+
+variable "region" {
+  type        = string
+  description = "The GCP region for resource deployment"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC network"
+}
+
+variable "k8s_pod_range" {
+  type        = string
+  description = "The CIDR range for Kubernetes pods"
+}
+
+variable "k8s_service_range" {
+  type        = string
+  description = "The CIDR range for Kubernetes services"
+}
+
+variable "ssh_username" {
+  type        = string
+  description = "The SSH username for the bastion host"
+}
+
+variable "ssh_key_path" {
+  type        = string
+  description = "The path to the SSH public key file"
+}
+
+variable "master_ipv4_cidr_block" {
+  type        = string
+  description = "The CIDR block for the Kubernetes master nodes"
+}
+
+variable "jenkins_cidr_block" {
+  type        = string
+  description = "The CIDR block for Jenkins server access"
+}
+
+variable "min_node_count" {
+  type        = number
+  description = "The minimum number of nodes in the GKE cluster"
+}
+
+variable "max_node_count" {
+  type        = number
+  description = "The maximum number of nodes in the GKE cluster"
+}
+
+variable "node_machine_type" {
+  type        = string
+  description = "The machine type for GKE nodes"
+}
+
+variable "ssh_private_key" {
+  type        = string
+  description = "The path to the SSH private key file"
+}
+
+variable "env" {
+  type        = string
+  description = "The environment (e.g., dev, prod)"
+}
