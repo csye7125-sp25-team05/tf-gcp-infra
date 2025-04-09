917cdfb14e82f49b5474175af646128bb0a2cbc6
Mayuresh Satao <msatao75@gmail.com>
2025-04-08 20:46:10 -0400

    feat: logging and monitoring

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -1,14 +1,7 @@
 variable "organization_id" {
   description = "The id of the organization"
   type        = string
-
 }
-
-# variable "project_name" {
-#   description = "The name of the project"
-#   type        = string
-
-# }
 
 variable "zones" {
   type    = list(string)
@@ -18,7 +11,6 @@ variable "zones" {
 variable "env" {
   description = "The environment"
   type        = string
-
 }
 
 variable "project_id" {
@@ -29,15 +21,12 @@ variable "project_id" {
 variable "billing_account" {
   description = "The billing account"
   type        = string
-
 }
 
 variable "ssh_key_path" {
   description = "The ssh key"
   type        = string
-
 }
-
 
 variable "cidr_block" {
   type = string
@@ -49,7 +38,6 @@ variable "k8s_pod_range" {
 
 variable "k8s_service_range" {
   type = string
-
 }
 
 variable "github_token" {
@@ -60,42 +48,34 @@ variable "github_token" {
 
 variable "ssh_username" {
   type = string
-
 }
 
 variable "region" {
   type = string
-
 }
 
 variable "master_ipv4_cidr_block" {
   type = string
-
 }
 
 variable "jenkins_cidr_block" {
   type = string
-
 }
 
 variable "min_node_count" {
   type = number
-
 }
 
 variable "max_node_count" {
   type = number
-
 }
 
 variable "node_machine_type" {
   type = string
-
 }
 
 variable "ssh_private_key" {
   type = string
-
 }
 
 variable "project_no" {
@@ -107,7 +87,7 @@ variable "project_no" {
 variable "local_ip" {
   type        = string
   description = "Your local machine's public IP for GKE control plane access"
-  default     = "0.0.0.0/0"  # Default is open to all (not recommended for production)
+  default     = "0.0.0.0/0" # Default is open to all (not recommended for production)
 }
 
 variable "istio_namespace" {
